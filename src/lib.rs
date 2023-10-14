95136faca31bfe31608a1af0cd0c4454f2daea89
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-10-14 19:01:44 -0400

    added TDStretch and uint exports
    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-pub use root::soundtouch::*;
+pub use root::{soundtouch::*, TDStretch, uint};
 
 #[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 mod root {
